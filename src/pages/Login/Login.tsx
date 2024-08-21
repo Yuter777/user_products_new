6953b60dc6f561e5a24597fe12556ba5de98b1f6
yuter777 <muhammad06tt@gmail.com>
2024-08-21 23:26:18 +0500

    bu

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,13 +1,13 @@
 import React from "react";
-import { Form, Input, Button, Checkbox } from "antd";
+import { Form, Input, Button, Checkbox, Space, Select } from "antd";
 import { UserOutlined, LockOutlined } from "@ant-design/icons";
 import { Link, useNavigate } from "react-router-dom";
 import { paths } from "../../router/paths";
 import { useTranslation } from "react-i18next";
+import i18n from "../../i18n";
 
 const Login: React.FC = () => {
   const navigate = useNavigate();
-
   const { t } = useTranslation();
 
   const onFinish = (values: any) => {
@@ -18,7 +18,20 @@ const Login: React.FC = () => {
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100">
       <div className="w-full max-w-md p-8 bg-white shadow-lg rounded-lg">
-        <h2 className="text-2xl font-semibold text-center mb-6">Login</h2>
+        <h2 className="text-2xl font-semibold text-center mb-6">
+          {t("Login")}
+        </h2>
+        <Space className="flex items-center pb-3 justify-center">
+          <Select
+            defaultValue={i18n.language}
+            onChange={(value) => i18n.changeLanguage(value)}
+            style={{ width: 120 }}
+          >
+            <Select.Option value="uz">O'zbekcha</Select.Option>
+            <Select.Option value="ru">Русский</Select.Option>
+            <Select.Option value="en">English</Select.Option>
+          </Select>
+        </Space>
         <Form
           name="login"
           initialValues={{ remember: true }}
@@ -26,14 +39,18 @@ const Login: React.FC = () => {
         >
           <Form.Item
             name="username"
-            rules={[{ required: true, message: "Please input your Username!" }]}
+            rules={[
+              { required: true, message: t("Please input your Username!") },
+            ]}
           >
             <Input prefix={<UserOutlined />} placeholder={t("Username")} />
           </Form.Item>
 
           <Form.Item
             name="password"
-            rules={[{ required: true, message: "Please input your Password!" }]}
+            rules={[
+              { required: true, message: t("Please input your Password!") },
+            ]}
           >
             <Input.Password
               prefix={<LockOutlined />}
@@ -49,13 +66,13 @@ const Login: React.FC = () => {
 
           <Form.Item>
             <Button type="primary" htmlType="submit" className="w-full">
-              Log in
+              {t("Log in")}
             </Button>
           </Form.Item>
           <div className="text-center">
-            <span>Don't have an account? </span>
+            <span>{t("Don't have an account?")} </span>
             <Link to={paths.SIGNUP} className="text-blue-500">
-              Sign Up
+              {t("Sign Up")}
             </Link>
           </div>
         </Form>
--- src/pages/SignUp/SignUp.tsx
+++ src/pages/SignUp/SignUp.tsx
@@ -1,49 +1,68 @@
-import { Form, Input, Button } from "antd";
+import { Form, Input, Button, Space, Select } from "antd";
 import { UserOutlined, LockOutlined, MailOutlined } from "@ant-design/icons";
 import { Link, useNavigate } from "react-router-dom";
 import { paths } from "../../router/paths";
+import { useTranslation } from "react-i18next";
+import i18n from "../../i18n";
 
 const SignUp: React.FC = () => {
   const navigate = useNavigate();
+  const { t } = useTranslation();
 
   const onFinish = (values: any) => {
     console.log("Received values of form: ", values);
-    // Ro'yxatdan o'tish logikasini shu yerda amalga oshirasiz
-    // Muvaffaqiyatli ro'yxatdan o'tgandan keyin login sahifasiga yo'naltirish
     navigate(paths.LOGIN);
   };
 
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100">
       <div className="w-full max-w-md p-8 bg-white shadow-lg rounded-lg">
-        <h2 className="text-2xl font-semibold text-center mb-6">Sign Up</h2>
+        <h2 className="text-2xl font-semibold text-center mb-6">
+          {t("Sign Up")}
+        </h2>{" "}
+        <Space className="flex items-center pb-3 justify-center">
+          <Select
+            defaultValue={i18n.language}
+            onChange={(value) => i18n.changeLanguage(value)}
+            style={{ width: 120 }}
+          >
+            <Select.Option value="uz">O'zbekcha</Select.Option>
+            <Select.Option value="ru">Русский</Select.Option>
+            <Select.Option value="en">English</Select.Option>
+          </Select>
+        </Space>
         <Form name="register" onFinish={onFinish}>
           <Form.Item
             name="email"
             rules={[
-              { required: true, message: "Please input your Email!" },
-              { type: "email", message: "Please enter a valid email!" },
+              { required: true, message: t("Please input your Email!") },
+              { type: "email", message: t("Please enter a valid email!") },
             ]}
           >
-            <Input prefix={<MailOutlined />} placeholder="Email" />
+            <Input prefix={<MailOutlined />} placeholder={t("Email")} />
           </Form.Item>
 
           <Form.Item
             name="username"
-            rules={[{ required: true, message: "Please input your Username!" }]}
+            rules={[
+              { required: true, message: t("Please input your Username!") },
+            ]}
           >
-            <Input prefix={<UserOutlined />} placeholder="Username" />
+            <Input prefix={<UserOutlined />} placeholder={t("Username")} />
           </Form.Item>
 
           <Form.Item
             name="password"
             rules={[
-              { required: true, message: "Please input your Password!" },
-              { min: 6, message: "Password must be at least 6 characters!" },
+              { required: true, message: t("Please input your Password!") },
+              { min: 6, message: t("Password must be at least 6 characters!") },
             ]}
             hasFeedback
           >
-            <Input.Password prefix={<LockOutlined />} placeholder="Password" />
+            <Input.Password
+              prefix={<LockOutlined />}
+              placeholder={t("Password")}
+            />
           </Form.Item>
 
           <Form.Item
@@ -51,14 +70,14 @@ const SignUp: React.FC = () => {
             dependencies={["password"]}
             hasFeedback
             rules={[
-              { required: true, message: "Please confirm your password!" },
+              { required: true, message: t("Please confirm your password!") },
               ({ getFieldValue }) => ({
                 validator(_, value) {
                   if (!value || getFieldValue("password") === value) {
                     return Promise.resolve();
                   }
                   return Promise.reject(
-                    new Error("The two passwords do not match!")
+                    new Error(t("The two passwords do not match!"))
                   );
                 },
               }),
@@ -66,19 +85,19 @@ const SignUp: React.FC = () => {
           >
             <Input.Password
               prefix={<LockOutlined />}
-              placeholder="Confirm Password"
+              placeholder={t("Confirm Password")}
             />
           </Form.Item>
 
           <Form.Item>
             <Button type="primary" htmlType="submit" className="w-full">
-              Register
+              {t("Register")}
             </Button>
           </Form.Item>
           <div className="text-center">
-            <span>Already have an account? </span>
+            <span>{t("Already have an account?")} </span>
             <Link to={paths.LOGIN} className="text-blue-500">
-              Log in
+              {t("Log in")}
             </Link>
           </div>
         </Form>
